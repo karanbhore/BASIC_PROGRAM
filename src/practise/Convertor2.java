427c5664762f75821029920e90f7a4dc829e35e7
karan <karanbhore64@gmail.com>
2023-04-23 17:21:21 +0530

    excercise

--- src/practise/Convertor2.java
+++ src/practise/Convertor2.java
@@ -1,5 +1,34 @@
 package practise;
 
+import java.util.Scanner;
+
 public class Convertor2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("type");
+
+		char op = sc.next().charAt(0);
+
+		System.out.println(op);
+
+		switch (op) {
+
+		case 'a':
+
+			System.out.println("hello");
+			break;
+
+		case 'b':
+			System.out.println("hi");
+			break;
+		default:
+			System.out.println("*");
+			break;
+		}
+
+	}
 
 }
